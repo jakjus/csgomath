45ab34f7edc61a6b144b229a2fb8c64c7a0eec04
idlehere <fazzebo@gmail.com>
2021-03-07 17:27:20 +0100

    a

--- steam_market_2.py
+++ steam_market_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[213]:
+# In[1]:
 
 
 import numpy as np
@@ -13,7 +13,6 @@ import pprint
 import pickle
 import datetime
 
-
 odds_rarity = {'4b69ff': 0.7992327, '8847ff': 0.1598465, 'd32ce6': 0.0319693, 'eb4b4b': 0.0063939, 'ffd700': 0.0025575}
 odds_wear = {'Factory New':0.1471, 'Minimal Wear':0.2468,'Field-Tested':0.4318,'Battle-Scarred':0.0993,'Well-Worn':0.0792}
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -23,7 +22,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleW
 sleeptime = 12
 
 
-# In[214]:
+# In[2]:
 
 
 # HELPER FUNCTIONS
@@ -31,41 +30,44 @@ sleeptime = 12
 def text_to_price(text):
     return int(text.replace('$','').replace(',','').replace('.',''))
 
-def check(page):
-    if page.status_code != 200:
-        print('Error: ', page.status_code)
+def check(url):
+    time.sleep(sleeptime)
+    page = requests.get(url, headers=headers)
+    while page.status_code != 200:
+        print('Error: ', page.status_code,'Retrying...')
+        page = requests.get(url, headers=headers)
+        time.sleep(sleeptime)
+    return page
 
 
-# In[216]:
+# In[3]:
 
 
 def get_csgo_items():
     l = []
     i = 0
     while True:
-        url = 'https://steamcommunity.com/market/search/render/?query=&start='+str(i*100)+'&count=100&appid=730&sort_column=name&sort_dir=asc&norender=1'
-        page = requests.get(url, headers=headers)
-        check(page)
+        url = 'https://steamcommunity.com/market/search/render/?query=&start='+str(i*100)+'&count=100&appid=730&norender=1'
+        page = check(url)
         d = page.json()
         l += d['results']
         if i == math.floor(int(d['total_count'])/100):
             break
         i += 1
-        time.sleep(sleeptime)
     return l
 
 items = get_csgo_items()
 print(len(items))
 
 
-# In[32]:
+# In[10]:
 
 
-#with open('steam_market_items.pickle', 'wb') as handle:
+#with open('steam_market_items2pickle', 'wb') as handle:
 #    pickle.dump(items, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-# In[218]:
+# In[6]:
 
 
 #pprint.pprint(items[0])
@@ -119,11 +121,8 @@ def add_sale_price(case_key_list):
 def add_chest_value(case_key_list):
     upd_ckl = []
     for case_key in case_key_list:
-        try:
-            res = get_estimated_chest_value(case_key[0]['asset_description']['descriptions'])
-        except Exception as e:
-            print('Error:',e,'Case:',case_key[0])
-
+        #print(case_key[0]['asset_description'])
+        res = get_ipython().run_line_magic('time', "get_estimated_chest_value(case_key[0]['asset_description']['descriptions'])")
         new_case = case_key[0]
         new_case['asset_description']['descriptions'] = res['new_desc']
         upd_ck = {'case': new_case, 'key': case_key[1], 'value':get_estimated_chest_value(case_key[0]['asset_description']['descriptions'])['total']}
@@ -157,7 +156,8 @@ def get_estimated_one_weapon_value(name):
             if found < 1:
                 total /= found
     except Exception as e:
-        print('Error with:',name, e)
+        print('Error:', e, name)
+        raise
     return round(total)
 
 def get_estimated_chest_value(desc):
@@ -172,21 +172,53 @@ def get_estimated_chest_value(desc):
         if 'color' in item.keys():
             if item['color'] in odds_rarity.keys():
                 if 'Special Item' in item['value']:
-                    item_value = get_many_weapon_value('Knife |')
+                    if 'Special Item' not in special_item_found:
+                        item_value = get_many_weapon_value('Knife |')
+                        special_item_found['Special Item'] = item_value
+                    else:
+                        item_value = special_item_found['Special Item']
                 elif 'Rare Gloves' in item['value']:
-                    item_value = get_many_weapon_value('Gloves')
+                    if 'Rare Gloves' not in special_item_found:
+                        item_value = get_many_weapon_value('Gloves')
+                        special_item_found['Rare Gloves'] = item_value
+                    else:
+                        item_value = special_item_found['Rare Gloves']
                 elif 'Falchion Knife' in item['value']:
-                    item_value = get_many_weapon_value('Falchion Knife')
+                    if 'Falchion Knife' not in special_item_found:
+                        item_value = get_many_weapon_value('Falchion Knife')
+                        special_item_found['Falchion Knife'] = item_value
+                    else:
+                        item_value = special_item_found['Falchion Knife']
                 elif 'Shadow Daggers' in item['value']:
-                    item_value = get_many_weapon_value('Shadow Daggers')
+                    if 'Shadow Daggers' not in special_item_found:
+                        item_value = get_many_weapon_value('Shadow Daggers')
+                        special_item_found['Shadow Daggers'] = item_value
+                    else:
+                        item_value = special_item_found['Shadow Daggers']
                 elif 'Bowie Knife' in item['value']:
-                    item_value = get_many_weapon_value('Bowie Knife')
+                    if 'Bowie Knife' not in special_item_found:
+                        item_value = get_many_weapon_value('Bowie Knife')
+                        special_item_found['Bowie Knife'] = item_value
+                    else:
+                        item_value = special_item_found['Bowie Knife']
                 elif 'Butterfly Knife' in item['value']:
-                    item_value = get_many_weapon_value('Butterfly Knife')
+                    if 'Butterfly Knife' not in special_item_found:
+                        item_value = get_many_weapon_value('Butterfly Knife')
+                        special_item_found['Butterfly Knife'] = item_value
+                    else:
+                        item_value = special_item_found['Butterfly Knife']
                 elif 'Classic Knife' in item['value']:
-                    item_value = get_many_weapon_value('Classic Knife')
+                    if 'Classic Knife' not in special_item_found:
+                        item_value = get_many_weapon_value('Classic Knife')
+                        special_item_found['Classic Knife'] = item_value
+                    else:
+                        item_value = special_item_found['Classic Knife']
                 elif 'Huntsman Knife' in item['value']:
-                    item_value = get_many_weapon_value('Huntsman Knife')
+                    if 'Huntsman Knife' not in special_item_found:
+                        item_value = get_many_weapon_value('Huntsman Knife')
+                        special_item_found['Huntsman Knife'] = item_value
+                    else:
+                        item_value = special_item_found['Huntsman Knife']
                 else:
                     item_value = get_estimated_one_weapon_value(item['value'])
                 item['total'] = item_value
@@ -216,21 +248,32 @@ def get_many_weapon_value(name):
         total += w
     return round(total/len(l))
         
+special_item_found = {}
 cases = filter_items_end('Case')
 keys = filter_items_end('Case Key')
+#print(cases[2])
 
+#print(get_estimated_one_weapon_value('★ Butterfly Knife | Marble Fade'))
+#print(list(map(lambda x: x['name'], filter_items('★ Butterfly Knife | Marble Fade'))))
 case_key_list = extract_case_key_list(cases, keys)
 case_key_list_sale = add_sale_price(case_key_list)
 case_key_list_value = add_chest_value(case_key_list_sale)
 #pprint.pprint(list(map(lambda x: (x['case']['name'],x['case']['desc'],x['value'],x['case']['sale_price']+x['key']['sale_price']), case_key_list_value)))
 
 
-# In[219]:
+# In[8]:
+
 
 
 url = 'http://localhost:3000/api/cases'
+
 final_caseobj = {'datetime': datetime.datetime.now().isoformat(), 'case_key_list_value':case_key_list_value}
-r = requests.post(url, json=case_key_list_value)
+r = requests.post(url, json=final_caseobj)
 print(r)
+
+
+# In[ ]:
+
+
 
 
