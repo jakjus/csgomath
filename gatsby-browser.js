27e060e62aedd7f5aac117bb520f735afbb7da99
idlehere <fazzebo@gmail.com>
2021-03-19 15:59:13 +0100

    a

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,3 @@
-exports.onServiceWorkerUpdateReady= () => window.location.reload(true);
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fontsource/lato";
 import "./src/layouts/index.css";
