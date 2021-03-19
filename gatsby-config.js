023081a523ab41c4b38d352d4ce2b0efdd2b0ea3
idlehere <fazzebo@gmail.com>
2021-03-19 15:41:53 +0100

    a

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     "gatsby-plugin-sitemap",
 		`gatsby-plugin-image`,
     "gatsby-transformer-sharp",
-		//`gatsby-plugin-fontawesome-css`,
+		`gatsby-plugin-fontawesome-css`,
 		{
       resolve: `gatsby-plugin-google-adsense`,
       options: {
@@ -26,20 +26,20 @@ module.exports = {
       },
       __key: "images",
     },
-		{
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `csgomath`,
-        short_name: `csgomath`,
-        start_url: `/`,
-        background_color: `#fafafa`,
-        theme_color: `#fafafa`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `standalone`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
-      },
-    },
+		// {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `csgomath`,
+    //     short_name: `csgomath`,
+    //     start_url: `/`,
+    //     background_color: `#fafafa`,
+    //     theme_color: `#fafafa`,
+    //     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+    //     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+    //     display: `standalone`,
+    //     icon: `src/images/icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
 		//`gatsby-plugin-offline`,
 		`gatsby-plugin-react-helmet`
   ],
